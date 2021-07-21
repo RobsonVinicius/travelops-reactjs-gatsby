38ebfff7c94b887430cc62cd371c9248310afe83
Robson <robsondgn@outlook.com>
2021-07-20 22:11:02 -0300

    pages cleaned

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
 import * as React from "react"
 import Email from "../components/Email"
 import Hero from "../components/Hero"
-// import { StaticImage } from "gatsby-plugin-image"
-
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import Seo from "../components/seo"
 import Stats from "../components/Stats"
 import Testimonials from "../components/Testimonials"
@@ -16,16 +14,7 @@ const IndexPage = () => (
     <Trips heading="Our Favorite Destionations" />
     <Testimonials />
     <Stats />
-    <Email />    
-    {/* <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    /> */}
-    
+    <Email />        
   </Layout>
 )
 
